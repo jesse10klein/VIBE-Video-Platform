55a2970838e1b8dea10a33fdab5a4e2970f4ff17
Jesse <jesse10klein@gmail.com>
2020-03-06 14:20:18 +1000

    added some styling to video page and fixed bugs

--- routes/video.js
+++ routes/video.js
@@ -13,11 +13,45 @@ router.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser');
 router.use(cookieParser());
 
+function formatDay(day) {
+  const ones = day % 10;
+  let formatted = "" + day;
+  switch (ones) {
+      case 1:
+          formatted += 'st';
+          break;
+      case 2: 
+          formatted += 'nd';
+          break;
+      case 3:
+          formatted += 'rd';
+          break;
+      default:
+          formatted += 'th';
+          break;
+  }
+  return formatted;
+}
+
+function formatDate(entry) {
+  let formattedDate = "";
+  var contents = entry.split('-');
+  console.log(contents);
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const month = months[parseInt(contents[1]) - 1];
+  const day = formatDay(parseInt(contents[2]));
+  const year = contents[0];
+  formattedDate += day + " " + month + " " + year;
+  return formattedDate;
+}
+
+
 function asyncHandler(cb) {
     return async(req, res, next) => {
       try {
         await cb(req, res, next);
       } catch(error) {
+        console.log(error);
         res.status(500).send(error.message);
       }
     }
@@ -81,8 +115,10 @@ router.post('/process-upload', asyncHandler(async (req, res) => {
 
 //Sorting comments under video
 router.get('/:id', asyncHandler(async (req, res) => {
+    console.log(res.locals.login);
 
-    const video = await Video.findByPk(req.params.id);
+    let video = await Video.findByPk(req.params.id);
+    const formattedDate = formatDate(video.uploadDate);
     const {username} = req.cookies;
     const comments = await Comments.findAll({ 
         order: [["createdAt", "DESC"]],
@@ -92,26 +128,31 @@ router.get('/:id', asyncHandler(async (req, res) => {
       where: {username: video.uploader}
     })
     //Check if user is subscribed to the uploader
-    const subscription = await Subscriptions.findOne({
-      where: {user: uploader.username, subscriber: username}
-    })
-    const subscribed = !(subscription == null);
-    res.render("videoViews/video-specific", {video, comments, uploader, username, subscribed});
+    let subscribed = false;
+    if (req.cookies.username) {
+      const subscription = await Subscriptions.findOne({
+        where: {user: uploader.username, subscriber: username}
+      })
+      subscribed = !(subscription == null);
+    }
+    res.render("videoViews/video-specific", {video, comments, uploader, username, subscribed, formattedDate});
 }));
 
 //Subscribe to a user
 router.get('/:videoID/subscribe', asyncHandler(async (req, res) => {
+
+  //Make sure user is logged in
+  if (req.cookies.username == null) {
+    console.log("About to redirect");
+    return res.redirect(`/video/${req.params.videoID}`);
+  }
+
   const video = await Video.findByPk(req.params.videoID);
   const user = video.uploader;
 
   const uploader = await UserInfo.findOne({where: { username: user }})
   const newSubCount = uploader.subscriberCount + 1;
   await uploader.update({ subscriberCount: newSubCount });
-
-  //Make sure user is logged in
-  if (req.cookies.username == null) {
-      res.send("You must login before you can subscribe to someone");
-  }
 
   const subscriber = req.cookies.username;
   await Subscriptions.create({ user, subscriber });
@@ -120,6 +161,12 @@ router.get('/:videoID/subscribe', asyncHandler(async (req, res) => {
 
 //Unsubscribe from a user
 router.get('/:videoID/unsubscribe', asyncHandler(async (req, res) => {
+
+  //Make sure user is logged in
+  if (req.cookies.username == null) {
+    return res.redirect(`/video/${req.params.videoID}`);
+  }
+
   const video = await Video.findByPk(req.params.videoID);
   const user = video.uploader;
 
@@ -127,12 +174,7 @@ router.get('/:videoID/unsubscribe', asyncHandler(async (req, res) => {
   const newSubCount = uploader.subscriberCount - 1;
   await uploader.update({ subscriberCount: newSubCount });
 
-  //Make sure user is logged in
-  if (req.cookies.username == null) {
-      res.send("You must login before you can unsubscribe to someone");
-  }
   const subscriber = req.cookies.username;
-
   const subscription = await Subscriptions.findOne({
     where: { user, subscriber }
   })
